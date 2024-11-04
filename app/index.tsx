b8485c786010e99a085452a5d21ea5c508d13dc5
hasan <mohdhasan7867214@gmail.com>
2024-11-04 21:11:42 +0530

    fixed status bar issue and added some UI fix

--- app/index.tsx
+++ app/index.tsx
@@ -39,13 +39,14 @@ export default function Index() {
     return () => backHandler.remove();
   }, [backPressCount]);
 
-
   const hideElements = `
     try {
       jQuery(document).ready(function($) {
         if ($(window).width() <= 768) {
           $('.component-wrap').css('display', 'none');
           $('.site-footer').css('display', 'none');
+          $('.neve-main').css('margin-bottom', '-12px');
+          $('.wp-container-core-group-is-layout-2').css('margin-bottom','-68px');
         }
       });
     } catch (error) {
@@ -62,62 +63,63 @@ export default function Index() {
   };
 
   return (
-      <View style={styles.container}>
-      <StatusBar style="light" backgroundColor="black" translucent={false} />
-        {isConnected ? (
-          <WebView
-            ref={webViewRef}
-            style={styles.flexContainer}
-            startInLoadingState={true}
-            renderLoading={() => (
-              <View style={styles.loadingContainer}>
-                <ActivityIndicator color="black" size="large" />
-              </View>
-            )}
-            source={{ uri: 'https://infosourcenews.com/' }}
-            injectedJavaScript={hideElements}
-            onLoadStart={() => webViewRef?.current?.injectJavaScript(hideElements)}
-            onError={() => {
-              console.log("error occured");
-              setIsConnected(false)
-            }}
-          />
-        ) : (
-          <View style={styles.errorContainer}>
-            <MaterialIcons name="wifi-off" size={48} color="#206795" />
-            <Text style={styles.errorMessage}>Check your internet connection</Text>
-            <TouchableOpacity onPress={handleReload} style={styles.retryButton}>
-              <Text style={styles.retryText}>Retry</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-        <Portal>
-          <Dialog visible={visible} onDismiss={hideDialog} style={{ backgroundColor: '#fff' }}>
-            <Dialog.Title style={{ color: '#004364' }}>Confirm Exit</Dialog.Title>
-            <Dialog.Content>
-              <Text style={{ color: '#000' }}>Are you sure you want to exit?</Text>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <PaperButton onPress={hideDialog} textColor="#004364" style={styles.button}>
-                No, Cancel
-              </PaperButton>
-              <PaperButton onPress={handleExit} textColor="#004364" style={styles.button}>
-                Yes, Exit
-              </PaperButton>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
-      </View>
+    <View style={styles.container}>
+      <StatusBar style="light" backgroundColor="black" translucent={true} />
+      {isConnected ? (
+        <WebView
+          ref={webViewRef}
+          style={styles.webView}
+          startInLoadingState={true}
+          renderLoading={() => (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator color="black" size="large" />
+            </View>
+          )}
+          source={{ uri: 'https://infosourcenews.com/' }}
+          injectedJavaScript={hideElements}
+          onLoadStart={() => webViewRef?.current?.injectJavaScript(hideElements)}
+          onError={() => {
+            console.log("error occurred");
+            setIsConnected(false);
+          }}
+        />
+      ) : (
+        <View style={styles.errorContainer}>
+          <MaterialIcons name="wifi-off" size={48} color="#206795" />
+          <Text style={styles.errorMessage}>Check your internet connection</Text>
+          <TouchableOpacity onPress={handleReload} style={styles.retryButton}>
+            <Text style={styles.retryText}>Retry</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog} style={{ backgroundColor: '#fff' }}>
+          <Dialog.Title style={{ color: '#004364' }}>Confirm Exit</Dialog.Title>
+          <Dialog.Content>
+            <Text style={{ color: '#000' }}>Are you sure you want to exit?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <PaperButton onPress={hideDialog} textColor="#004364" style={styles.button}>
+              No, Cancel
+            </PaperButton>
+            <PaperButton onPress={handleExit} textColor="#004364" style={styles.button}>
+              Yes, Exit
+            </PaperButton>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    backgroundColor: '#022e42',
   },
-  flexContainer: {
+  webView: {
     flex: 1,
+    marginTop: Constants.statusBarHeight,
   },
   loadingContainer: {
     position: 'absolute',
